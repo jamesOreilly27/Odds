2acde9882c9841fd38a45cdd1385db8cde050cca
James O'Reilly <James.Oreilly926@gmail.com>
2019-07-10 13:09:43 -0400

    set up basic alignment for golf odds banner

--- client/Components/Match.js
+++ client/Components/Match.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FlexRowContainer } from './baseComponents'
+import { FlexRowContainer, FlexColumnContainer } from './baseComponents'
 
-// const Wrapper = styled(FlexRowContainer)`
-//   background-color: blue;
-//   color: white
-// `
+const Wrapper = styled(FlexColumnContainer)`
+  font-size: 14px;
+  width: 12vw;
+  height: 6vw;
+  margin: 1vw;
+`
+
+const GolfNameContainer = styled(FlexColumnContainer)`
+
+`
+
+const GolfLineContainer = styled.div`
+
+`
+
+const splitName = string => string.split(' ')
 
 const Match = props => (
-  <div>
-    Hello World
-  </div>
+  <Wrapper>
+    {console.log(splitName(props.details.Participant.Name))}
+    <GolfNameContainer>
+      <div>{splitName(props.details.Participant.Name)[0]}</div>
+      <div>{splitName(props.details.Participant.Name)[1]}</div>
+    </GolfNameContainer>
+
+    <GolfLineContainer>
+      {`+${props.details.MoneyLineHome}`}
+    </GolfLineContainer>
+  </Wrapper>
 )
 
 export default Match
