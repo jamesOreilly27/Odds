0159f8358bb2e38d76c36fcb4b03331c2aace372
James O'Reilly <James.Oreilly926@gmail.com>
2019-07-23 15:03:19 -0400

    create 'upsert' function that creates or updates a game with data from jsonodds

--- server/routes/odds.js
+++ server/routes/odds.js
@@ -4,6 +4,17 @@ const chalk = require('chalk')
 const { Game } = require('../db/models')
 const jsonOdds = require('./jsonOdds')
 
+const upsert = (values, condition) => {
+  return Game.findOne({
+    where: condition
+  })
+  .then(game => {
+    if(game) { return game.update(values) }
+    else { return Game.create(values) }
+  })
+  .catch(err => console.log(err))
+}
+
 router.get('/:sport', (req, res, next) => {
   const sportString = req.params.sport
   jsonOdds.get(`https://jsonodds.com/api/odds/${sportString}?oddType=Game`)
@@ -22,8 +33,20 @@ router.get('/:sport/games', (req, res, next) => {
 })
 
 router.post('/:sport/games', (req, res, next) => {
-  const game = {}
-  res.json({ taste: 'tendy', margret: 'thatcher' })
+  const game = {
+    MatchId: req.body.ID,
+    MatchTime: req.body.MatchTime,
+    HomeTeam: req.body.HomeTeam,
+    AwayTeam: req.body.AwayTeam,
+    Final: false
+  }
+  // Game.create(game)
+  // .then(newGame => res.json(newGame))
+  // .catch(next)
+
+  upsert(game, { MatchId: game.MatchId })
+  .then(game => res.json(game))
+  .catch(next)
 })
 
 module.exports = router
