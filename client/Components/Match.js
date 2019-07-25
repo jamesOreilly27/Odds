c6795918bf850e804f79709851ae411bffe8e2da
James O'Reilly <James.Oreilly926@gmail.com>
2019-07-25 11:40:07 -0400

    remove obsolete styled components in the Match component

--- client/Components/Match.js
+++ client/Components/Match.js
@@ -6,20 +6,6 @@ import { TeamMatch, Participant } from '../Components'
 import { TeamGameWrapper, TeamGameHeader, TeamGameDetails, DetailContainer, NameLogoContainer, TeamOddsContainer } from './TeamSportsStyledComponents'
 import { processTime, truncateTeamName, addPlus } from './helpers'
 
-const Wrapper = styled(FlexColumnContainer)`
-  margin-left: 20px;
-  height: 100%;
-  padding-bottom: 10px;
-`
-
-const GolfNameContainer = styled(FlexRowContainer)`
-
-`
-
-const GolfLineContainer = styled.div`
-
-`
-
 class Match extends Component {
   constructor(props) {
     super(props)
