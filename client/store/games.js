bbc1117c1748b31c60abf11a53a74b414361a24e
James O'Reilly <James.Oreilly926@gmail.com>
2019-07-27 11:54:47 -0400

    add redux infrastructure for getting non final games

--- client/store/games.js
+++ client/store/games.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { API_URL } from '../../secrets'
 
 const GOT_GAMES = 'GOT_GAMES'
+const GOT_NON_FINAL_GAMES = 'GOT_NON_FINAL_GAMES'
 const GOT_FINAL_GAMES = 'GOT_FINAL_GAMES'
 
 const gotGames = games => ({
@@ -13,21 +15,33 @@ const gotFinalGames = games => ({
   payload: games
 })
 
+const gotNonFinalGames = games => ({
+  type: GOT_NON_FINAL_GAMES,
+  payload: games
+})
+
 export const getGamesThunk = sport => dispatch =>
-  axios.get(`/api/margretthatcher/tastytendies/odds/${sport}/games`)
+  axios.get(`${API_URL}/odds/${sport}/games`)
   .then(res => dispatch(gotGames(res.data)))
   .catch(err => dispatch(gotGames(err)))
 
 export const getFinalGames = sport => dispatch =>
-  axios.get(`/api/margretthatcher/tastytendies/odds/${sport}/final`)
+  axios.get(`${API_URL}/odds/${sport}/final`)
   .then(res => dispatch(gotFinalGames(res.data)))
   .catch(err => dispatch(gotFinalGames(err)))
+
+export const getNonFinalGames = sport => dispatch =>
+  axios.get(`${API_URL}/odds/${sport}/nonfinal`)
+  .then(res => dispatch(gotNonFinalGames(res.data)))
+  .catch(err => dispatch(gotNonFinalGames(err)))
 
 const reducer = (games = [], action) => {
   switch(action.type) {
     case GOT_GAMES:
       return action.payload
     case GOT_FINAL_GAMES:
+      return action.payload
+    case GOT_NON_FINAL_GAMES:
       return action.payload
     default:
       return games
