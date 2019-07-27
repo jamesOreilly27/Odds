a9e402a0ebd93ae1795e8c531d3dadf76177c46f
James O'Reilly <James.Oreilly926@gmail.com>
2019-07-27 11:36:51 -0400

    add redux infrastructure for getting all final games

--- client/store/games.js
+++ client/store/games.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
 const GOT_GAMES = 'GOT_GAMES'
+const GOT_FINAL_GAMES = 'GOT_FINAL_GAMES'
 
 const gotGames = games => ({
   type: GOT_GAMES,
+  payload: games
+})
+
+const gotFinalGames = games => ({
+  type: GOT_FINAL_GAMES,
   payload: games
 })
 
@@ -12,9 +18,16 @@ export const getGamesThunk = sport => dispatch =>
   .then(res => dispatch(gotGames(res.data)))
   .catch(err => dispatch(gotGames(err)))
 
+export const getFinalGames = sport => dispatch =>
+  axios.get(`/api/margretthatcher/tastytendies/odds/${sport}/final`)
+  .then(res => dispatch(gotFinalGames(res.data)))
+  .catch(err => dispatch(gotFinalGames(err)))
+
 const reducer = (games = [], action) => {
   switch(action.type) {
     case GOT_GAMES:
+      return action.payload
+    case GOT_FINAL_GAMES:
       return action.payload
     default:
       return games
