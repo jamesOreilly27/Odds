edf52675debfed3ece8e45509c90374117c93f93
James O'Reilly <James.Oreilly926@gmail.com>
2019-08-23 19:04:05 -0400

    remove shopify server file

--- server/shopify.js
+++ server/shopify.js
@@ -1,48 +1,48 @@
-const router = require('express').Router()
-const nonce = require('nonce')
-const chalk = require('chalk')
-const shopifyMiddleware = require('shopify-node-api')
+// const router = require('express').Router()
+// const nonce = require('nonce')
+// const chalk = require('chalk')
+// const shopifyMiddleware = require('shopify-node-api')
 
-const fowardingAddress = 'https://5468435a.ngrok.io'
-const shopify_hidden_ss = process.env.SHOPIFY_API_SECRET_KEY
-const shopify_hidden_ak = process.env.SHOPIFY_API_KEY
-const shopify_hidden_shopname = "dbny-code-test.myshopify.com"
-const randomString = nonce()
-let shopify_hidden_at = ''
+// const fowardingAddress = 'https://5468435a.ngrok.io'
+// const shopify_hidden_ss = process.env.SHOPIFY_API_SECRET_KEY
+// const shopify_hidden_ak = process.env.SHOPIFY_API_KEY
+// const shopify_hidden_shopname = "dbny-code-test.myshopify.com"
+// const randomString = nonce()
+// let shopify_hidden_at = ''
 
-const config = {
-  shop: shopify_hidden_shopname,
-  shopify_api_key: shopify_hidden_ak,
-  shopify_shared_secret: shopify_hidden_ss,
-  shopify_scope: 'write_products',
-  redirect_uri: `${fowardingAddress}/shopify/finish_auth`,
-  nonce: randomString
-}
+// const config = {
+//   shop: shopify_hidden_shopname,
+//   shopify_api_key: shopify_hidden_ak,
+//   shopify_shared_secret: shopify_hidden_ss,
+//   shopify_scope: 'write_products',
+//   redirect_uri: `${fowardingAddress}/shopify/finish_auth`,
+//   nonce: randomString
+// }
 
-router.get('/', (req, res) => {
-  const Shopify = new shopifyMiddleware(config)
+// router.get('/', (req, res) => {
+//   const Shopify = new shopifyMiddleware(config)
 
-  const auth_url = Shopify.buildAuthURL();
-  res.redirect(auth_url)
-})
+//   const auth_url = Shopify.buildAuthURL();
+//   res.redirect(auth_url)
+// })
 
-router.get('/finish_auth', (req, res) => {
-  const queryParams = req.query
+// router.get('/finish_auth', (req, res) => {
+//   const queryParams = req.query
 
-  const Shopify = new shopifyMiddleware({
-    shop: shopify_hidden_shopname,
-    shopify_api_key: shopify_hidden_ak,
-    shopify_shared_secret: shopify_hidden_ss,
-    shopify_scope: 'write_products',
-    redirect_uri: `${fowardingAddress}/shopify/finish_auth`,
-    nonce: queryParams.state
-  })
+//   const Shopify = new shopifyMiddleware({
+//     shop: shopify_hidden_shopname,
+//     shopify_api_key: shopify_hidden_ak,
+//     shopify_shared_secret: shopify_hidden_ss,
+//     shopify_scope: 'write_products',
+//     redirect_uri: `${fowardingAddress}/shopify/finish_auth`,
+//     nonce: queryParams.state
+//   })
 
-  // console.log(chalk.white.bgGreen(Shopify.is_valid_signature(queryParams)))
+//   // console.log(chalk.white.bgGreen(Shopify.is_valid_signature(queryParams)))
 
-  Shopify.exchange_temporary_token(queryParams, function(error, data) { 
-    shopify_hidden_at=data['access_token']
-  })
-})
+//   Shopify.exchange_temporary_token(queryParams, function(error, data) { 
+//     shopify_hidden_at=data['access_token']
+//   })
+// })
 
-module.exports = router
+// module.exports = router
