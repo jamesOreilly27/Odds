3c0d0822f98ac73d8de09bd328984a52cfa40fb9
James O'Reilly <James.Oreilly926@gmail.com>
2019-10-31 14:19:00 -0400

    update startListening function

--- server/app.js
+++ server/app.js
@@ -44,14 +44,11 @@ const createApp = () => {
 }
 
 const startListening = () => {
-  // const run = app.listen(PORT, () => console.log(chalk.blue.bgWhite.bold(`We are live on port ${PORT}`)))
   if(process.env.PORT) {
     app.listen(PORT, () => console.log(chalk.red.bgWhite.bold(`We are live on port ${PORT}`)))
   } else {
     server.listen(PORT, () => console.log(chalk.blue.bgWhite.bold(`We are live on port ${server.address().port}`)))
   }
-  // app.listen(PORT, () => console.log(chalk.red.bgWhite.bold(`We are live on port ${PORT}`)))
-  // server.listen(PORT, () => console.log(chalk.blue.bgWhite.bold(`We are live on port ${server.address().port}`)))
 }
 
 // app.listen(PORT, () => console.log(chalk.red.bgWhite.bold(`We are live on port ${PORT}`)))
