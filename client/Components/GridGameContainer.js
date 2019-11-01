946b04f9b79babd7e9b811066c8cbf7da1ef767d
James O'Reilly <James.Oreilly926@gmail.com>
2019-11-01 15:35:14 -0400

    create team containers for each grid item

--- client/Components/GridGameContainer.js
+++ client/Components/GridGameContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FlexColumnContainer, FlexRowContainer } from './baseComponents'
+import { processDayMonthTime } from './helpers'
 
 const Wrapper = styled(FlexRowContainer)`
   @media(max-width: 960px) {
@@ -14,9 +15,47 @@ const Wrapper = styled(FlexRowContainer)`
   height: 30vh;
 `
 
+const Container = styled(FlexColumnContainer)`
+  justify-content: flex-start;
+  width: 100%;
+  height: 100%;
+  padding: 15px 20px;
+`
+
+const GameDate = styled(FlexRowContainer)`
+  margin-bottom: 8px;
+`
+
+const TeamsContainer = styled(FlexRowContainer)`
+  width: 90%;
+  justify-content: space-between;
+`
+
+const Team = styled(FlexColumnContainer)`
+  height: 25vh;
+  border: 1px dotted red;
+  width: 45%;
+`
+
+const TeamName = styled(FlexColumnContainer)`
+
+`
+
 const GridGameContainer = ({ activeSport, match }) => (
   <Wrapper>
-    {match.HomeTeam}
+    <Container>
+      <GameDate>
+        {match.HomeScore === null ? 
+          processDayMonthTime(match)
+          :
+          <div>In Progress</div>
+        }
+      </GameDate>
+      <TeamsContainer>
+        <Team>{match.AwayTeam}</Team>
+        <Team>{match.HomeTeam}</Team>
+      </TeamsContainer>
+    </Container>
   </Wrapper>
 )
 
