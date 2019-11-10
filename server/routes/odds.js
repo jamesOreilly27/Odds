4b2343c44786418e783811a332190002dfcfc1d8
Ben Selland <sellandb@gmail.com>
2019-11-10 10:53:36 -0500

    Implemented Caching in Odds and Results Calls

--- server/routes/odds.js
+++ server/routes/odds.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const chalk = require('chalk')
 const { Game } = require('../db/models')
 const jsonOdds = require('./jsonOdds')
+const nodecache = require('node-cache')
+const cache = new nodecache({stdTTL: 60})
 
 const upsert = (values, condition) => {
   return Game.findOne({
@@ -35,10 +37,19 @@ const chooseId = obj => {
 
 router.get('/:sport', (req, res, next) => {
   const sportString = req.params.sport
-  jsonOdds.get(`https://jsonodds.com/api/odds/${sportString}?oddType=Game`)
-  .then(res => res.data)
-  .then(details => res.json(details))
-  .catch(console.error)
+  let apiURL = `https://jsonodds.com/api/odds/${sportString}?oddType=Game`
+  let apiData = cache.get(apiURL)
+  if(apiData){
+    res.json(apiData)
+  } else {
+    jsonOdds.get(apiURL)
+    .then(res => res.data)
+    .then(function (details) {
+      res.json(details)
+      cache.set(apiURL, details)
+    })
+    .catch(console.error)
+  }
 })
 
 router.get('/:sport/games', (req, res, next) => {
--- server/routes/results.js
+++ server/routes/results.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const chalk = require('chalk')
 const jsonOdds = require('./jsonOdds')
+const nodecache = require('node-cache')
+const cache = new nodecache({stdTTL: 60})
 
 router.get('/:sport', (req, res, next) => {
   const sportString = req.params.sport
-  jsonOdds.get(`https://jsonodds.com/api/results/${sportString}?oddType=game`)
-  .then(res => res.data)
-  .then(details => res.json(details))
-  .catch(console.error)
+  let apiURL = `https://jsonodds.com/api/odds/${sportString}?oddType=Game`
+  let apiData = cache.get(apiURL)
+  if(apiData){
+    res.json(apiData)
+  } else {
+    jsonOdds.get(apiURL)
+    .then(res => res.data)
+    .then(function (details) {
+      res.json(details)
+      cache.set(apiURL, details)
+    })
+    .catch(console.error)
+  }
 })
 
 module.exports = router
