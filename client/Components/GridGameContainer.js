da56525ca2188c6f962b81a86327cad8186618cc
James O'Reilly <James.Oreilly926@gmail.com>
2019-11-12 15:04:19 -0500

    remove 'props' from the new code. Coponents are deconstructed

--- client/Components/GridGameContainer.js
+++ client/Components/GridGameContainer.js
@@ -91,7 +91,7 @@ const GridGameContainer = ({ activeSport, match }) => (
           <NameAndLogo>
             <LogoContainer>
               {truncateTeamName(activeSport, match.AwayTeam) && 
-                <Logo src={'/assets/production/' + props.activeSport + '-logos/' + truncateTeamName(props.activeSport, props.match.AwayTeam) + '.png'} />
+                <Logo src={'/assets/production/' + activeSport + '-logos/' + truncateTeamName(activeSport, match.AwayTeam) + '.png'} />
               }
             </LogoContainer>
             <TeamName>
@@ -120,7 +120,7 @@ const GridGameContainer = ({ activeSport, match }) => (
           <NameAndLogo>
             <LogoContainer>
               {truncateTeamName(activeSport, match.HomeTeam) && 
-                <Logo src={'/assets/production/' + props.activeSport + '-logos/' + truncateTeamName(props.activeSport, props.match.HomeTeam) + '.png'} />
+                <Logo src={'/assets/production/' + activeSport + '-logos/' + truncateTeamName(activeSport, match.HomeTeam) + '.png'} />
               }
             </LogoContainer>
             <TeamName>
